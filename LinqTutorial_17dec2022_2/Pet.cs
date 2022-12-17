fc931b2c40f891f4a7cc6aec9c99c7a73bd5b11d
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-17 07:29:33 +0100

    Add project files.

---
+++ LinqTutorial_17dec2022_2/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTutorial_17dec2022_2;
+
+public enum PetType
+{
+    Dog,
+    Cat,
+    Fish
+}
+internal class Pet
+{
+    public Pet(int id, string name, PetType type, float weight)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Weight = weight;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public PetType Type { get; set; }
+    public float Weight { get; set; }
+}
---
+++ LinqTutorial_17dec2022_2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LinqTutorial_17dec2022_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var pets = new[]
+        {
+            new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+            new Pet(2, "Anthony", PetType.Cat, 2f),
+            new Pet(3, "Ed", PetType.Cat, 0.7f),
+            new Pet(4, "Taiga", PetType.Dog, 35f),
+            new Pet(5, "Rex", PetType.Dog, 40f),
+            new Pet(6, "Lucky", PetType.Dog, 5f),
+            new Pet(7, "Storm", PetType.Cat, 0.9f),
+            new Pet(8, "Nyan", PetType.Cat, 2.2f)
+        };
+
+        var countOfDogs = pets.Count(pet => pet.Type == PetType.Dog);
+        Console.WriteLine($"{nameof(countOfDogs)}: {countOfDogs}");
+
+        var countOfPetsNamedBruce = pets.LongCount(pet => pet.Name == "Bruce");
+        Console.WriteLine($"{nameof(countOfPetsNamedBruce)}: {countOfPetsNamedBruce}");
+
+        var countOfAllSmallDogs = pets.LongCount(pet => pet.Weight < 10 && pet.Type == PetType.Dog);
+        Console.WriteLine($"{nameof(countOfAllSmallDogs)}: {countOfAllSmallDogs}");
+
+        var allPetsCount = pets.Count();
+        Console.WriteLine($"{nameof(allPetsCount)}: {allPetsCount}");
+
+    }
+}
